33796bbd78d588d6e94820bbf75fc94e38072198
Datamaverik <verma.pranshu160705@gmail.com>
2024-03-26 22:33:12 +0530

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color =
+    score > 80
+      ? "green"
+      : score > 60
+      ? "yellow"
+      : score > 50
+      ? "orange"
+      : "red";
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius={4}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
