66a437dd78c776db97a5140626cf1a0246e7ebc9
Datamaverik <verma.pranshu160705@gmail.com>
2024-03-27 01:14:24 +0530

    Display genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageURL from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+        <ListItem key={data.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius="8px"
+              src={getCroppedImageURL(data.image_background)}
+            ></Image>
+            <Text fontSize='lg'>{data.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
